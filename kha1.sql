a8d2b29406aed5d75ceba6f704a143af44200cf2
Vũ Minh Trung <167429293+trung1609@users.noreply.github.com>
2025-11-01 21:53:01 +0700

    Delete kha1.sql

--- kha1.sql
+++
@@ -1,9 +0,0 @@
-create schema library
-
-create table library.Books(
-	book_id serial primary key,
-	title varchar(100) not null,
-	author varchar(50) not null,
-	published_year int,
-	price numeric(10,2)
-);
